4a4e2036656636c3717022fd361487e2a29f576b
sergunya95 <sergunya95@gmail.com>
2021-03-01 17:05:43 +0700

    Initial Commit

---
+++ URL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URL
+//
+//  Created by apple on 3/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
