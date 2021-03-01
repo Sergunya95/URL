f60c23d8a5bde7a991e243f87358feeae3254ca1
sergunya95 <sergunya95@gmail.com>
2021-03-01 17:38:12 +0700

    download image from net

--- URL/ViewController.swift
+++ URL/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var getImageButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        activityIndicator.isHidden = true
+        activityIndicator.hidesWhenStopped = true
     }
 
-
+    @IBAction func getImagePressed(_ sender: Any) {
+        
+        label.isHidden = true
+        getImageButton.isEnabled = false
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let url = URL(string: "https://i.pinimg.com/564x/8d/3c/ea/8d3ceaf69c0f1ec5ace57ed5ddecceac.jpg") else { return }
+        
+        let session = URLSession.shared
+        
+        session.dataTask(with: url) { (data, responce, error) in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.activityIndicator.stopAnimating()
+                    self.imageView.image = image
+                }
+            }
+        }.resume()
+    }
+    
 }
 
